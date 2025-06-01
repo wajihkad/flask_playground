b2e4a4032db431a593f8c761d4998cf3c1642d42
wajih KADDAMI <wkaddami@gmail.com>
2025-06-01 18:53:56 +0000

    add html template and css

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
-app = Flask(__name__)
+from . import pages
 
-@app.route("/")
-def home():
-    return "Hello, World!"
+def create_app():
+    app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.register_blueprint(pages.bp)
+    return app
